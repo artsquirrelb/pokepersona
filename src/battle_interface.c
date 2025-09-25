187b8d58cadf0760c7eef2b78c9a54292e7e465a
artsquirrelb <lengnxuanphuong@gmail.com>
2025-09-26 01:46:00 +0700

    revert textbox color change and fix script bugs
    still mainhall rich man and lady bugs and riverbank tileset bug

--- src/battle_interface.c
+++ src/battle_interface.c
@@ -2068,12 +2068,9 @@ void UpdateHealthboxAttribute(u8 healthboxSpriteId, struct Pokemon *mon, u8 elem
 
 s32 MoveBattleBar(u8 battler, u8 healthboxSpriteId, u8 whichBar, u8 unused)
 {
-    //s32 currentBarValue;
-    u32 s;
-    u32 speedScale = Rogue_GetBattleSpeedScale(TRUE);
-    s32 currentBarValue = 0;
+    s32 currentBarValue;
 
-    /*if (whichBar == HEALTH_BAR) // health bar
+    if (whichBar == HEALTH_BAR) // health bar
     {
         u16 hpFraction = B_FAST_HP_DRAIN == FALSE ? 1 : max(gBattleSpritesDataPtr->battleBars[battler].maxValue / (B_HEALTHBAR_PIXELS / 2), 1);
         currentBarValue = CalcNewBarValue(gBattleSpritesDataPtr->battleBars[battler].maxValue,
@@ -2082,44 +2079,20 @@ s32 MoveBattleBar(u8 battler, u8 healthboxSpriteId, u8 whichBar, u8 unused)
                     &gBattleSpritesDataPtr->battleBars[battler].currValue,
                     B_HEALTHBAR_PIXELS / 8, hpFraction);
     }
-    else // exp bar*/
-    for(s = 0; s < speedScale; ++s)
+    else // exp bar
     {
-        /*u16 expFraction = GetScaledExpFraction(gBattleSpritesDataPtr->battleBars[battler].oldValue,
+        u16 expFraction = GetScaledExpFraction(gBattleSpritesDataPtr->battleBars[battler].oldValue,
                     gBattleSpritesDataPtr->battleBars[battler].receivedValue,
                     gBattleSpritesDataPtr->battleBars[battler].maxValue, 8);
         if (expFraction == 0)
             expFraction = 1;
         expFraction = abs(gBattleSpritesDataPtr->battleBars[battler].receivedValue / expFraction);
-*/
-if (whichBar == HEALTH_BAR) // health bar
-        {
-            currentBarValue = CalcNewBarValue(gBattleSpritesDataPtr->battleBars[battler].maxValue,
-                        gBattleSpritesDataPtr->battleBars[battler].oldValue,
-                        gBattleSpritesDataPtr->battleBars[battler].receivedValue,
-                        &gBattleSpritesDataPtr->battleBars[battler].currValue,
-                        B_HEALTHBAR_PIXELS / 8, 1);
-        }
-        else // exp bar
-        {
-            // Instant
-            if (gBattleSpritesDataPtr->battleBars[battler].currValue == -32768) // first function call
-            {
-                gBattleSpritesDataPtr->battleBars[battler].currValue = gBattleSpritesDataPtr->battleBars[battler].receivedValue;
-            }
-            else
-            {
-                currentBarValue = -1;
-            }
-        /*currentBarValue = CalcNewBarValue(gBattleSpritesDataPtr->battleBars[battler].maxValue,
+
+        currentBarValue = CalcNewBarValue(gBattleSpritesDataPtr->battleBars[battler].maxValue,
                     gBattleSpritesDataPtr->battleBars[battler].oldValue,
                     gBattleSpritesDataPtr->battleBars[battler].receivedValue,
                     &gBattleSpritesDataPtr->battleBars[battler].currValue,
-                    B_EXPBAR_PIXELS / 8, expFraction);*/
-        }
-
-        if(currentBarValue == -1)
-            break;
+                    B_EXPBAR_PIXELS / 8, expFraction);
     }
 
     if (whichBar == EXP_BAR || (whichBar == HEALTH_BAR && !gBattleSpritesDataPtr->battlerData[battler].hpNumbersNoBars))
@@ -2317,8 +2290,8 @@ static u8 CalcBarFilledPixels(s32 maxValue, s32 oldValue, s32 receivedValue, s32
     return filledPixels;
 }
 
-//static u8 GetScaledExpFraction(s32 oldValue, s32 receivedValue, s32 maxValue, u8 scale)
-static u8 UNUSED GetScaledExpFraction(s32 oldValue, s32 receivedValue, s32 maxValue, u8 scale)
+static u8 GetScaledExpFraction(s32 oldValue, s32 receivedValue, s32 maxValue, u8 scale)
+//static u8 UNUSED GetScaledExpFraction(s32 oldValue, s32 receivedValue, s32 maxValue, u8 scale)
 {
     s32 newVal, result;
     s8 oldToMax, newToMax;

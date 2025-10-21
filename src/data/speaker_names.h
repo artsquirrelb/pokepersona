d087f53df36fd8df5e5a3ed3d14383591d71a771
artsquirrelb <lengnxuanphuong@gmail.com>
2025-10-22 02:11:45 +0700

    edited some scripts format

--- src/data/speaker_names.h
+++ src/data/speaker_names.h
@@ -4,6 +4,6 @@ const u8 *const gSpeakerNamesTable[SP_NAME_COUNT] =
     [SP_NAME_MITSURU] = COMPOUND_STRING("Mitsuru"),
     [SP_NAME_SHINJIRO] = COMPOUND_STRING("Shinjiro"),
     [SP_NAME_MIKI] = COMPOUND_STRING("Miki"),
-    [SP_NAME_KOBAYASHI] = COMPOUND_STRING("Ms. Kobayashi"),
+    [SP_NAME_KOBAYASHI] = COMPOUND_STRING("Kobayashi"),
     [SP_NAME_KUROSAWA] = COMPOUND_STRING("Kurosawa"),
 };

d9900d8f3d20ce1669b5957cf8d267b344ed806b
artsquirrelb <lengnxuanphuong@gmail.com>
2025-11-30 18:11:18 +0700

    finalize its going down now + add complete dppt voicegroups

--- src/heat_start_menu.c
+++ src/heat_start_menu.c
@@ -1313,7 +1313,7 @@ static void ShowSaveInfoWindow(void) {
     
     BufferSaveMenuText(SAVE_MENU_LOCATION, gStringVar4, 0x0);
     //AddTextPrinterParameterized(sSaveInfoWindowId, FONT_NORMAL, gStringVar4, 0, yOffset, TEXT_SKIP_DRAW, NULL);
-    AddTextPrinterParameterized6(sSaveInfoWindowId, FONT_NORMAL, gStringVar4, 8, yOffset, TEXT_SKIP_DRAW, NULL, 1, 0x0, 0x0);
+    AddTextPrinterParameterized6(sSaveInfoWindowId, FONT_NARROW, gStringVar4, 8, yOffset, TEXT_SKIP_DRAW, NULL, 1, 0x0, 0x0);
 
     // Print player name
     yOffset += 16;
